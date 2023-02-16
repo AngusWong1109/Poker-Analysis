d5364345410da364c15f1569ba5ffb7444b6354a
angus350 <ccw21@sfu.ca>
2023-02-16 10:57:17 -0800

    starting on TextUI

--- src/UI/TextUI.java
+++ src/UI/TextUI.java
@@ -1,7 +1,28 @@
 package UI;
 
-public class TextUI {
-    public void start(){
+import model.Card;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class TextUI {
+    private static int bettingPool = 0;
+    private static List<Card> cardList = new ArrayList<>();
+
+    private static final int SUIT_AMOUNT = 4;
+    private static final int NUMBER_AMOUNT = 13;
+    public void start(){
+        setCardList();
+
+    }
+
+    private void setCardList() {
+        Card card;
+        for(short suit = 1; suit <= SUIT_AMOUNT; suit++){
+            for(int number = 1; number <= NUMBER_AMOUNT; number++){
+                card = new Card(suit, number);
+                cardList.add(card);
+            }
+        }
     }
 }
--- src/model/Card.java
+++ src/model/Card.java
@@ -23,4 +23,6 @@ public class Card {
     public int getNumber() {
         return number;
     }
+
+
 }
